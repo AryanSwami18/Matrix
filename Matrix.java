9ae8f915184225e2b61b55a49c1c3bcc566bb460
Aryan <114349788+AryanSwami18@users.noreply.github.com>
2023-08-08 13:08:35 +0530

    Now it calculates proper determinant and added transpose function

--- Matrix.java
+++ Matrix.java
@@ -1,27 +1,38 @@
 class Matrix{
-    public static int determinant(int[][] mat,int r,int c){
-        int d = 0;
-        if(r == 1 && c == 1){
-            d = mat[0][0];
-        }
-        else if(r == 2 && c ==2){
-            d = (mat[0][0]*mat[1][1]) - (mat[1][0]*mat[0][1]);
-        }
-        else{
-            for(int skipcol = 0;skipcol<r;skipcol++){
-                int[][] newMatrix = new int[r-1][c-1];
-                for(int row = 0;row<r-1;row++){
-                    int matcol = 0;
-                    for(int col = 0;col<c-1;col++){
-                        if(col != skipcol ){
-                            newMatrix[row][matcol++] = mat[row+1][col];
-                        }
+    public static int determinant(int[][] mat, int r, int c) {
+    int d = 0;
+    if (r == 1 && c == 1) {
+        d = mat[0][0];
+    } else if (r == 2 && c == 2) {
+        d = (mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]);
+    } else {
+        int sign = 1;
+        for (int skipcol = 0; skipcol < r; skipcol++) {
+            int[][] newMatrix = new int[r - 1][c - 1];
+            for (int row = 1; row < r; row++) {
+                int matcol = 0;
+                for (int col = 0; col < c; col++) {
+                    if (col != skipcol) {
+                        newMatrix[row - 1][matcol++] = mat[row][col];
                     }
-                    d +=  mat[0][skipcol]*determinant(newMatrix, row, matcol);
                 }
             }
-
+            d += sign * mat[0][skipcol] * determinant(newMatrix, r - 1, c - 1);
+            sign = -sign;
         }
+    }
     return d;
+}
+
+   
+    public static int[][] transpose(int mat[][] ,int r,int c){
+        int[][] newMat = new int [r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                newMat[j][i] = mat[i][j];
+            }
+        }
+       
+        return newMat;
     }
 }
