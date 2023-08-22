3017e7b1de2db4a020049ee88275d40dd388f444
Aryan <114349788+AryanSwami18@users.noreply.github.com>
2023-08-22 23:58:07 +0530

    Added Cramers Rule

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 class Main {
+
+    public static void printt(double[] t){
+        System.out.println("vlaue are:");
+        for(int i=0;i<t.length;i++){
+                System.out.println(t[i]);
+        } 
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter Rows");
@@ -12,8 +19,17 @@ class Main {
                 matrix[i][j] = s.nextInt();
             }
         } 
-        int determinat = Matrix.determinant(matrix, rows, cols);
+        double determinat = Matrix.determinant(matrix, rows, cols);
         System.out.println("Determinant:"+determinat);
+        int equation[] = new int[matrix.length];
+        System.out.println("Enter "+matrix.length+" Coeff:");
+        for(int i=0;i<matrix.length;i++){
+            equation[i] = s.nextInt();
+        }
+
+        double values[] = Matrix.cramersRule(matrix, equation);
+
+        printt(values);
         s.close();
     }
 }
--- Matrix.java
+++ Matrix.java
@@ -1,6 +1,6 @@
 class Matrix{
-    public static int determinant(int[][] mat, int r, int c) {
-    int d = 0;
+    public static double determinant(int[][] mat, int r, int c) {
+    double d = 0;
     if (r == 1 && c == 1) {
         d = mat[0][0];
     } else if (r == 2 && c == 2) {
@@ -26,7 +26,7 @@ class Matrix{
 
    
     public static int[][] transpose(int mat[][] ,int r,int c){
-        int[][] newMat = new int [r][c];
+        int[][] newMat = new int [c][r];
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 newMat[j][i] = mat[i][j];
@@ -35,4 +35,30 @@ class Matrix{
        
         return newMat;
     }
+
+
+    public static double[] cramersRule(int mat[][], int equation[]) {
+        double values[] = new double[mat.length];
+        double d = determinant(mat, mat.length, mat[0].length);
+
+        if (d != 0) {
+            for (int chgCol = 0; chgCol < mat.length; chgCol++) {
+                int[][] temp = new int[mat.length][mat[0].length];
+
+                for (int rows = 0; rows < mat.length; rows++) {
+                    for (int cols = 0; cols < mat.length; cols++) {
+                        if (cols == chgCol) {
+                            temp[rows][cols] = equation[rows];
+                        } else {
+                            temp[rows][cols] = mat[rows][cols];
+                        }
+                    }
+                }
+                // printt(temp);
+                values[chgCol] = determinant(temp, temp.length, temp[0].length) / d;
+            }
+        }
+        return values;
+    }
+
 }
